1a128bd0e7261a52b9c0c2b6bc8e8a48fd631fb6
Zian-Andrew-GONG <ziangong0714@gmail.com>
2023-04-27 16:46:20 +0800

    add fmt & spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "Hello.h"
+#include "fmt/color.h"
+#include "spdlog/spdlog.h"
 
 int main(int argc, char *argv[])
 {
     Hello hi;
     hi.print();
+    fmt::print(fg(fmt::terminal_color::cyan), "Hello!\n");
+    spdlog::info("hello");
     return 0;
 }
